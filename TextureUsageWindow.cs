ddc1f0659022073ab550cce5308e27613c669698
Robert Rumney <robertrumney@gmail.com>
2023-09-23 12:35:06 +0200

    Update TextureUsageWindow.cs

--- TextureUsageWindow.cs
+++ TextureUsageWindow.cs
@@ -1,50 +1,66 @@
 using UnityEngine;
 using UnityEditor;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 
 public class TextureUsageWindow : EditorWindow
 {
+    // The scroll position for the texture list
     private Vector2 scrollPos;
-    private Dictionary<Texture, int> textureMemoryUsage = new Dictionary<Texture, int>();
 
+    // Dictionary to store each texture and its memory usage
+    private Dictionary<Texture, long> textureMemoryUsage = new Dictionary<Texture, long>();
+
+    // Menu item to open the Texture Usage window
     [MenuItem("Window/Texture Usage")]
     public static void ShowWindow()
     {
         GetWindow<TextureUsageWindow>("Texture Usage");
     }
 
-    void OnGUI()
+    private void OnGUI()
     {
+        // Button to fetch and display texture usage
         if (GUILayout.Button("Get Texture Usage"))
         {
             GetTextureUsage();
         }
 
+        // Begin a scroll view for the texture list
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, false);
-        
+
+        // Sort the textures by their memory usage in descending order
         var sortedTextureUsage = textureMemoryUsage.OrderByDescending(x => x.Value).ToList();
-        
-        foreach (KeyValuePair<Texture, int> entry in sortedTextureUsage)
+
+        // Display each texture and its memory usage
+        foreach (KeyValuePair<Texture, long> entry in sortedTextureUsage)
         {
             EditorGUILayout.BeginHorizontal();
             EditorGUILayout.ObjectField(entry.Key, typeof(Texture), false);
-            EditorGUILayout.LabelField(" | Memory (KB): " + entry.Value / 1024f);
+            EditorGUILayout.LabelField(" | Memory (KB): " + entry.Value / 1024.0); // Convert bytes to KB
             EditorGUILayout.EndHorizontal();
         }
 
+        // End the scroll view
         EditorGUILayout.EndScrollView();
     }
 
-    void GetTextureUsage()
+    // Method to fetch texture memory usage
+    private void GetTextureUsage()
     {
+        // Clear the existing data
         textureMemoryUsage.Clear();
 
+        // Iterate over all textures in the project
         foreach (Texture texture in Resources.FindObjectsOfTypeAll<Texture>())
         {
-            if(!string.IsNullOrEmpty(AssetDatabase.GetAssetPath(texture))) // Ensure it's an asset and not an internal texture
+            // Only consider textures that are assets (i.e., not internal or temporary textures)
+            if (!string.IsNullOrEmpty(AssetDatabase.GetAssetPath(texture)))
             {
-                int memSize = UnityEngine.Profiling.Profiler.GetRuntimeMemorySizeLong(texture);
+                // Get the memory size of the texture in bytes
+                long memSize = UnityEngine.Profiling.Profiler.GetRuntimeMemorySizeLong(texture);
+
+                // Store the texture and its memory size in the dictionary
                 textureMemoryUsage[texture] = memSize;
             }
         }
