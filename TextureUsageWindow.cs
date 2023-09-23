aaac071fd19c1f25ddfc12a410d7d48ab3b2fe22
Robert Rumney <robertrumney@gmail.com>
2023-09-23 13:45:49 +0200

    Update TextureUsageWindow.cs

--- TextureUsageWindow.cs
+++ TextureUsageWindow.cs
@@ -1,37 +1,46 @@
 using UnityEngine;
 using UnityEditor;
+
 using System.Linq;
 using System.Collections.Generic;
 
 public class TextureUsageWindow : EditorWindow
 {
+    // The position for the scroll view in the Editor window.
     private Vector2 scrollPos;
+
+    // Dictionary to hold each texture and its corresponding memory usage.
     private Dictionary<Texture, long> textureMemoryUsage = new Dictionary<Texture, long>();
 
     [MenuItem("Window/Texture Usage")]
     public static void ShowWindow()
     {
+        // Open the TextureUsageWindow.
         GetWindow<TextureUsageWindow>("Texture Usage");
     }
 
     private void OnGUI()
     {
+        // Button to fetch texture memory usage.
         if (GUILayout.Button("Get Texture Usage"))
         {
             GetTextureUsage();
         }
 
+        // Start of the scroll view.
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, false);
 
+        // Sort the texture usage by memory consumption.
         var sortedTextureUsage = textureMemoryUsage.OrderByDescending(x => x.Value).ToList();
 
+        // Display each texture and its memory usage.
         foreach (KeyValuePair<Texture, long> entry in sortedTextureUsage)
         {
             EditorGUILayout.BeginHorizontal();
             EditorGUILayout.ObjectField(entry.Key, typeof(Texture), false);
             EditorGUILayout.LabelField(" | Memory (KB): " + entry.Value / 1024.0);
 
-            // Button to Ping objects in the scene using this texture
+            // Button to locate game objects in the scene using this texture.
             if (GUILayout.Button("Ping in Scene"))
             {
                 PingObjectsUsingTexture(entry.Key);
@@ -40,15 +49,20 @@ public class TextureUsageWindow : EditorWindow
             EditorGUILayout.EndHorizontal();
         }
 
+        // End of the scroll view.
         EditorGUILayout.EndScrollView();
     }
 
+    // Populates the textureMemoryUsage dictionary with memory usage of each texture in the project.
     private void GetTextureUsage()
     {
+        // Clear the previous texture memory usage data.
         textureMemoryUsage.Clear();
 
+        // Go through all the textures in the project.
         foreach (Texture texture in Resources.FindObjectsOfTypeAll<Texture>())
         {
+            // Ensure the texture has a path in the AssetDatabase.
             if (!string.IsNullOrEmpty(AssetDatabase.GetAssetPath(texture)))
             {
                 long memSize = UnityEngine.Profiling.Profiler.GetRuntimeMemorySizeLong(texture);
@@ -57,27 +71,34 @@ public class TextureUsageWindow : EditorWindow
         }
     }
 
+    // Locates and selects all game objects in the scene that are using the targetTexture.
     private void PingObjectsUsingTexture(Texture targetTexture)
     {
+        // List to store game objects using the target texture.
         List<GameObject> objectsUsingTexture = new List<GameObject>();
+
+        // Fetch all renderers in the scene.
         Renderer[] renderers = GameObject.FindObjectsOfType<Renderer>();
 
+        // Go through each renderer to check if it uses the target texture.
         foreach (Renderer renderer in renderers)
         {
-            if (renderer.sharedMaterials != null) // Check if materials are assigned to the renderer
+            // Ensure the renderer has materials assigned.
+            if (renderer.sharedMaterials != null)
             {
                 foreach (Material mat in renderer.sharedMaterials)
                 {
+                    // Check if the material uses the target texture as its main texture.
                     if (mat != null && mat.HasProperty("_MainTex") && mat.mainTexture == targetTexture)
                     {
                         objectsUsingTexture.Add(renderer.gameObject);
-                        break; // break out of the inner loop as we've found a match for this renderer
+                        break;
                     }
                 }
             }
         }
 
-        // Select the found objects in the scene
+        // Set the Unity selection to the found game objects.
         Selection.objects = objectsUsingTexture.ToArray();
     }
 }
