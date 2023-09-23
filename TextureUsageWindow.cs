4ce0a384a3f6eabea118d0fb4314aeb3fcd7f884
Robert Rumney <robertrumney@gmail.com>
2023-09-23 13:35:47 +0200

    Update TextureUsageWindow.cs

--- TextureUsageWindow.cs
+++ TextureUsageWindow.cs
@@ -5,13 +5,9 @@ using System.Collections.Generic;
 
 public class TextureUsageWindow : EditorWindow
 {
-    // The scroll position for the texture list
     private Vector2 scrollPos;
-
-    // Dictionary to store each texture and its memory usage
     private Dictionary<Texture, long> textureMemoryUsage = new Dictionary<Texture, long>();
 
-    // Menu item to open the Texture Usage window
     [MenuItem("Window/Texture Usage")]
     public static void ShowWindow()
     {
@@ -20,49 +16,68 @@ public class TextureUsageWindow : EditorWindow
 
     private void OnGUI()
     {
-        // Button to fetch and display texture usage
         if (GUILayout.Button("Get Texture Usage"))
         {
             GetTextureUsage();
         }
 
-        // Begin a scroll view for the texture list
         scrollPos = EditorGUILayout.BeginScrollView(scrollPos, false, false);
 
-        // Sort the textures by their memory usage in descending order
         var sortedTextureUsage = textureMemoryUsage.OrderByDescending(x => x.Value).ToList();
 
-        // Display each texture and its memory usage
         foreach (KeyValuePair<Texture, long> entry in sortedTextureUsage)
         {
             EditorGUILayout.BeginHorizontal();
             EditorGUILayout.ObjectField(entry.Key, typeof(Texture), false);
-            EditorGUILayout.LabelField(" | Memory (KB): " + entry.Value / 1024.0); // Convert bytes to KB
+            EditorGUILayout.LabelField(" | Memory (KB): " + entry.Value / 1024.0);
+
+            // Button to Ping objects in the scene using this texture
+            if (GUILayout.Button("Ping in Scene"))
+            {
+                PingObjectsUsingTexture(entry.Key);
+            }
+
             EditorGUILayout.EndHorizontal();
         }
 
-        // End the scroll view
         EditorGUILayout.EndScrollView();
     }
 
-    // Method to fetch texture memory usage
     private void GetTextureUsage()
     {
-        // Clear the existing data
         textureMemoryUsage.Clear();
 
-        // Iterate over all textures in the project
         foreach (Texture texture in Resources.FindObjectsOfTypeAll<Texture>())
         {
-            // Only consider textures that are assets (i.e., not internal or temporary textures)
             if (!string.IsNullOrEmpty(AssetDatabase.GetAssetPath(texture)))
             {
-                // Get the memory size of the texture in bytes
                 long memSize = UnityEngine.Profiling.Profiler.GetRuntimeMemorySizeLong(texture);
-
-                // Store the texture and its memory size in the dictionary
                 textureMemoryUsage[texture] = memSize;
             }
         }
+    }
+
+    private void PingObjectsUsingTexture(Texture targetTexture)
+    {
+        List<GameObject> objectsUsingTexture = new List<GameObject>();
+        Renderer[] renderers = GameObject.FindObjectsOfType<Renderer>();
+
+        foreach (Renderer renderer in renderers)
+        {
+            if (renderer.sharedMaterials != null) // Check if materials are assigned to the renderer
+            {
+                foreach (Material mat in renderer.sharedMaterials)
+                {
+                    if (mat != null && mat.HasProperty("_MainTex") && mat.mainTexture == targetTexture)
+                    {
+                        objectsUsingTexture.Add(renderer.gameObject);
+                        break; // break out of the inner loop as we've found a match for this renderer
+                    }
+                }
+            }
+        }
+
+        // Select the found objects in the scene
+        Selection.objects = objectsUsingTexture.ToArray();
     }
 }
